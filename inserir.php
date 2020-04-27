19ed05433ec170a41a397be3631564921a878d46
andrer54 <andrer54@gmail.com>
2020-04-27 16:31:19 -0300

    fixing 1 list

--- inserir.php
+++ inserir.php
@@ -4,19 +4,19 @@ session_start();
 if($_SESSION['login'] == "andre"){
 ?>
 <?php
-header("Location: listar.php");
 
-$conexao = mysqli_connect("localhost", "root") or die ("Não pude conectar: ".mysql_error()); //usuario e senha do msql
-$base = mysqli_select_db($conexao, "infpessoais") or die ("Não pude selecionar o banco de dados");
+
+$conexao = mysqli_connect("localhost", "andre", "12345") or die("não pude conectar: "->mysqli_error());
+$base = mysqli_select_db($conexao, "crud-teste") or die("Não pude selecionar banco de dados");
 
 $nome = $_POST['nome'];
 $telefone = $_POST['telefone'];
 
 $query="INSERT INTO informacoes(id_contato, nome, telefone) VALUES(null, '$nome', '$telefone')";
 mysqli_query($conexao, $query);
-?>
 
-<?php
+header("Location: listar.php");
+
 }else{
     echo "sem permissoes para acessar a pagina.";
 }
--- listar.php
+++ listar.php
@@ -1,49 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
 ob_start();
 session_start();
-if($_SESSION['login'] == "andre"){
-?>
-<?php
+if ($_SESSION['login'] == "andre") {
 
-// PDO + MySQL
-//$pdo = new PDO('mysql:host=localhost;dbname=infpessoais', 'root', '');
-//$statement = $pdo->query("SELECT * FROM informacoes");
-//$row = $statement->fetch(PDO::FETCH_ASSOC);
-//echo htmlentities($row['nome']);
-
-https://databases.000webhost.com
+    //    https: //databases.000webhost.com
 
 
-$conexao = mysqli_connect("localhost", "id7183299_admin","Relogio1") or die("não pude conectar: ".mysql_error());
-$base=mysqli_select_db($conexao , "id7183299_master") or die ("Não pude selecionar banco de dados");
-?>
+    $conexao = mysqli_connect("localhost", "andre", "12345") or die("não pude conectar: "->mysqli_error());
+    $base = mysqli_select_db($conexao, "crud-teste") or die("Não pude selecionar banco de dados");
 
-<table align="center" border="1" cellspacing="0" cellpadding="0" bordercolor="#000000">
-    <tr align="center" bgcolor="#778899"><td><center><b><font color="#FFFFFF" size="-1">id</font></b></center></td><td><center>b<font color="#FFFFFF" size="-1">nome
-</b></center></td><td><center><b><font color="#FFFFFFF" size="-1">Telefone</font></b></center></td></tr>
+    $query = "SELECT * FROM clientes";
+    $resultado = mysqli_query($conexao, $query); ?>
+    <div>
+        <table>
+            <tr>
+                <th>id</th>
+                <th>Nome</th>
+                <th>Sobrenome</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+            <?php
 
-<?php
-$query="SELECT * FROM informacoes";
+            while ($linha = mysqli_fetch_row($resultado)) {
+            ?>
 
-$resultado=mysqli_query($conexao, $query);
 
-while ($linha=mysqli_fetch_row($resultado))
+                <tr>
+                    <td><?php echo $linha[0]; ?></td>
+                    <td><?php echo $linha[1]; ?></td>
+                    <td><?php echo $linha[2]; ?></td>
+                    <td><?php echo $linha[3]; ?></td>
+                    <td><?php echo $linha[4]; ?></td>
+                    <td><a href="form_editar.php?id=<?php echo $linha[0]; ?>">Editar</td>
+                    <td><a href="excluir.php?id=<?php echo $linha[0]; ?>">Excluir</td>
+                </tr>
 
-{
-    ?>
 
-    <tr bgcolor="#D6D3D3">
-        <td><?php echo $linha[0]; ?></td>
-        <td><?php echo $linha[1]; ?></td>
-        <td><?php echo $linha[2]; ?></td>
-        <td><a href="form_editar.php?id=<?php echo $linha[0]; ?>">Editar</td>
-        <td><a href="excluir.php?id=<?php echo $linha[0]; ?>">Excluir</td>
-</tr>
+        <?php
+            }
+        } else {
+            echo "Sem permissões para acessar a página!";
+        }
+        ?>
 
-<?php
-}?> </table>
-<?php
-}else{
-    echo "Sem permissões para acessar a página!";
-}
-?>
+        </table>
+    </div>
+
+    </body>
+</html>
